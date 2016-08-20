c85aa103e29ba3ad9f9c97c9b797369010756595
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-20 03:04:01 -0500

    Update color_green.js

--- rv/color_green.js
+++ rv/color_green.js
@@ -1,4 +1,4 @@
 var miCanvass = document.getElemetById("miCanvas");
 var context = miCanvass.getContext("2d");
-contexto.fillStyle = "green";
+context.fillStyle = "green";
 context.fillRect(530, 530, 50, 50);
