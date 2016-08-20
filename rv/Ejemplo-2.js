d10ecdc2a04e081ee8dcd4b2f0eedd86040999e1
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-20 02:42:56 -0500

    Update Ejemplo-2.js

--- rv/Ejemplo-2.js
+++ rv/Ejemplo-2.js
@@ -1,4 +1,4 @@
 var miCanvas = document.getElementById("miCanvas");
 var context = miCanvas.getContext("2d");
-contexto.fillStyle = "red";
+context.fillStyle = "red";
 context.fillRect(30, 30, 50, 50);
