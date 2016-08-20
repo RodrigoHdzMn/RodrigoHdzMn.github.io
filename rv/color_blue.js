5e7226a73f9b69a0d205220e5ee2e3af38b5ed1d
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-20 12:31:21 -0500

    Update color_blue.js

--- rv/color_blue.js
+++ rv/color_blue.js
@@ -1,4 +1,4 @@
 var miCanvas = document.getElementById("miCanvas");
 var context = miCanvas.getContext("2d");
 context.fillStyle = "blue";
-context.fillRect(30, 30, 50, 50);
+context.fillRect(195, 80, 100, 100);
