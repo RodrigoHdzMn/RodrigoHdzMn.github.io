6f8d3e6162a0b4402f2835b314c09586de31ec58
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-20 03:07:32 -0500

    Update color_red.js

--- rv/color_red.js
+++ rv/color_red.js
@@ -1,4 +1,4 @@
-var miCanvass = document.getElemetById("miCanvas");
+var miCanvass = document.getElementById("miCanvas");
 var context = miCanvass.getContext("2d");
 context.fillStyle = "red";
 context.fillRect(330, 330, 50, 50);
