d8546a49988df37754376e84ec77e5bdd1170018
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-23 15:43:58 -0500

    Update ejemplo-geometry.js

--- rv/ejemplo-geometry.js
+++ rv/ejemplo-geometry.js
@@ -7,8 +7,8 @@ forma.vertices.push( new THREE.Vector3(-1, 0, -1));
 forma.vertices.push( new THREE.Vector3(-1, 0, 1));
 forma.vertices.push( new THREE.Vector3(0, 1, 0));
 
-
-forma.faces.push(new THREE.Face3(3,2,1));<!-- Vertice3, vertice 2, vertice 1-- >
+<!--Vertice3, vertice 2, vertice 1-->
+forma.faces.push(new THREE.Face3(3,2,1)); 
 forma.faces.push(new THREE.Face3(3,1,0));
 forma.faces.push(new THREE.Face3(3,0,4));
 forma.faces.push(new THREE.Face3(0,1,4));
