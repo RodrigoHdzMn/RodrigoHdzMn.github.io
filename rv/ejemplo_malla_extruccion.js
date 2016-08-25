36cc8b6d00d45fdf41c6572c68aa994683321f8e
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-25 15:26:05 -0500

    Update ejemplo_malla_extruccion.js

--- rv/ejemplo_malla_extruccion.js
+++ rv/ejemplo_malla_extruccion.js
@@ -8,4 +8,12 @@ figura.lineTo(10,40);
 figura.lineTo(40,40);
 figura.lineTo(10,10);
 
+var forma= new THREE.ExtrudeGeometry(figura,{amount:10});
+//amoun->literal de objeto 
+
+var material=new THREE.MeshNormalMaterial();
+var malla=new THREE.Mesh(forma,material);
+malla.rotateX(Math.PI/4);
+
+
 
