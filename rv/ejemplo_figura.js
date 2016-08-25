18a22b6f84630041fe32b675908d51dfcce75da1
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-25 15:05:40 -0500

    Update ejemplo_figura.js

--- rv/ejemplo_figura.js
+++ rv/ejemplo_figura.js
@@ -16,8 +16,8 @@ escena.add(malla);
 var camara= new THREE.PerpectiveCamera();
 camara.position.z=100;
 
-var renderizador=new THREE.WebGLRender();
-renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95,);
+var renderizador=new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
