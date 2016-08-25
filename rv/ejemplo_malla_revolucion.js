605b70bc6099aaa3dcf4552ef7f067f7e25e6cfe
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-25 15:45:22 -0500

    Create ejemplo_malla_revolucion.js

---
+++ rv/ejemplo_malla_revolucion.js
@@ -0,0 +1,31 @@
+
+//MALLA POR REVOLUCION
+
+var puntos =[];
+
+//se insertan 50 puntos en el arreglo con diversos valores
+for (var i=0; i<50 ; i++)
+{
+    puntos.push( new THREE.Vector2(Math.sin(i*0.2)*15+50, (i-5)*2) );
+}
+
+//se define la forma, el material y la malla
+var forma = new THREE.LatheGeometry(puntos);
+
+var material=new THREE.MeshNormalMaterial();
+
+var malla=new THREE.Mesh(forma,material);
+malla.rotateX(Math.PI/6);
+
+var escena= new THREE.Scene();
+escena.add(malla);
+
+var camara= new THREE.PerspectiveCamera();
+camara.position.z=500;
+
+var renderizador=new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
+
