10e5b1673f722534cfd12e81785efd7fa87d08fa
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-26 18:18:48 -0500

    Update Ejemplo-4.js

--- rv/Ejemplo-4.js
+++ rv/Ejemplo-4.js
@@ -9,12 +9,12 @@ renderizador.setSize( window.innerHeight*.95,
 			
 document.body.appendChild (renderizador.domElement);
 
-var forma = new THREE.CircleGeometry (1,64);//radio,num de triangulos
+var forma = new THREE.CircleGeometry (0.1,64);//radio,num de triangulos
 var material = new THREE.MeshNormalMaterial(); 
 var circulo = new THREE.Mesh(forma, material);
 
 //cubo.rotateX(-Math.PI/4); 
-circulo.rotateY(Math.PI/4);
+//circulo.rotateY(Math.PI/4);
 escena.add(circulo);
 
 renderizador.render(escena,camara);
