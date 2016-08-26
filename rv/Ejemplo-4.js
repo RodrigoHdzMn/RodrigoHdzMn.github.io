d34ab0b3eb27c977a095c0685bd476084c599660
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-26 18:09:34 -0500

    Update Ejemplo-4.js

--- rv/Ejemplo-4.js
+++ rv/Ejemplo-4.js
@@ -9,7 +9,7 @@ renderizador.setSize( window.innerHeight*.95,
 			
 document.body.appendChild (renderizador.domElement);
 
-var forma = new THREE.CircleGeometry (1,3);
+var forma = new THREE.CircleGeometry (1,32);
 var material = new THREE.MeshNormalMaterial(); 
 var circulo = new THREE.Mesh(forma, material);
 
