2b96e51a69559c9a2a952a45082baedf1a426b24
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-26 18:30:41 -0500

    Update Ejemplo-4.js

--- rv/Ejemplo-4.js
+++ rv/Ejemplo-4.js
@@ -1,7 +1,7 @@
 var escena = new THREE.Scene();
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 20;
 camara.position.y = 2;
 
 var renderizador = new THREE.WebGLRenderer();
