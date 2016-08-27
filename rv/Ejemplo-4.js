c6c41e7508c6bcc32cfc7dc32566287b28c166db
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-27 16:15:31 -0500

    Update Ejemplo-4.js

--- rv/Ejemplo-4.js
+++ rv/Ejemplo-4.js
@@ -2,8 +2,6 @@ var escena = new THREE.Scene();
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 10;
-//camara.position.y = 1;
-
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.75,
@@ -11,17 +9,13 @@ renderizador.setSize( window.innerHeight*.75,
 			
 document.body.appendChild (renderizador.domElement);
 
-var forma2 = new THREE.BoxGeometry (1,4);
-var material_2 = new THREE.MeshBasicMaterial({color: 0x75461c}); 
-var rectangulo = new THREE.Mesh(forma2, material_2);
-forma2.translate(0,-0.5,0);
-escena.add(rectangulo);
+var forma = new THREE.BoxGeometry( 1,1,1);
+var material = new THREE.MeshNormalMaterial(); 
+var cubo = new THREE.Mesh(forma, material);
 
+cubo.rotateX(-Math.PI/4);
+cubo.rotateY(Math.PI/4);
 
-var forma = new THREE.SphereGeometry( 1.5, 32, 32 );
-var material_1 = new THREE.MeshBasicMaterial({color: 0x0adc6d}); 
-var circulo = new THREE.Mesh(forma, material_1);
-forma.translate(0,1.5,0);
-escena.add(circulo);
+escena.add(cubo);
 
 renderizador.render(escena,camara);
