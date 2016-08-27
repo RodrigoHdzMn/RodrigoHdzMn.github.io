020c03f7e393ea9b46c1fbd55cd99974ae4a824e
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-26 20:00:23 -0500

    Update Ejemplo-4.js

--- rv/Ejemplo-4.js
+++ rv/Ejemplo-4.js
@@ -12,11 +12,12 @@ renderizador.setSize( window.innerHeight*.95,
 document.body.appendChild (renderizador.domElement);
 
 var forma = new THREE.CircleGeometry (1,64);//radio,num de triangulos
-var material = new THREE.MeshNormalMaterial(); 
-var circulo = new THREE.Mesh(forma, material);
+var material_1 = new THREE.MeshNormalMaterial({color: 0x8B4513}); 
+var circulo = new THREE.Mesh(forma, material_1);
 
 var forma2 = new THREE.BoxGeometry (1,4);
-var rectangulo = new THREE.Mesh(forma2, material);
+var material_2 = new THREE.MeshNormalMaterial({color: 0x8B4513}); 
+var rectangulo = new THREE.Mesh(forma2, material_2);
 
 escena.add(rectangulo);
 escena.add(circulo);
