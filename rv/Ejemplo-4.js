8b74b1ee4eaae99fbe7cd9b2a173cdcc44c8284d
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-27 16:19:40 -0500

    Update Ejemplo-4.js

--- rv/Ejemplo-4.js
+++ rv/Ejemplo-4.js
@@ -4,8 +4,8 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.75,
-                    	window.innerHeight*.75);
+renderizador.setSize( window.innerHeight*.95,
+                    	window.innerHeight*.95);
 			
 document.body.appendChild (renderizador.domElement);
 
