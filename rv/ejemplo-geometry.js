7b7eb2711c05b1bb02c9c53806f8511e33baa33c
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-27 16:42:19 -0500

    Update ejemplo-geometry.js

--- rv/ejemplo-geometry.js
+++ rv/ejemplo-geometry.js
@@ -1,4 +1,4 @@
-<!-- -->
+
 var forma=new THREE.Geometry();
 
 forma.vertices.push( new THREE.Vector3(1, 0, 1));
@@ -7,7 +7,7 @@ forma.vertices.push( new THREE.Vector3(-1, 0, -1));
 forma.vertices.push( new THREE.Vector3(-1, 0, 1));
 forma.vertices.push( new THREE.Vector3(0, 1, 0));
 
-<!--Vertice3, vertice 2, vertice 1-->
+//Vertice3, vertice 2, vertice 1
 forma.faces.push(new THREE.Face3(3,2,1)); 
 forma.faces.push(new THREE.Face3(3,1,0));
 forma.faces.push(new THREE.Face3(3,0,4));
@@ -15,9 +15,9 @@ forma.faces.push(new THREE.Face3(0,1,4));
 forma.faces.push(new THREE.Face3(1,2,4));
 forma.faces.push(new THREE.Face3(2,3,4));
 
-<!-- Se solicita el computo de la esfera de menor tamaño que acota al objeto-->
+//Se solicita el computo de la esfera de menor tamaño que acota al objeto
 forma.computeBoundingSphere();
-<!--Tipo de material al usar, se computan las normales de las caras -->
+//Tipo de material al usar, se computan las normales de las caras 
 forma.computeFaceNormals();
 
 var material= new THREE.MeshNormalMaterial();
