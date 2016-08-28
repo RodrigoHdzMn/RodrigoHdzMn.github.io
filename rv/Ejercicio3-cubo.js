3dfb6f676d557872e071f802b172f3d1e62064a4
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-27 19:40:57 -0500

    Update Ejercicio3-cubo.js

--- rv/Ejercicio3-cubo.js
+++ rv/Ejercicio3-cubo.js
@@ -11,18 +11,18 @@ forma.vertices.push(  new THREE.Vector3(1,-1,-1) );
 forma.vertices.push(  new THREE.Vector3(-1,-1,-1) );
 
 //Contruyendo las caras con los vértices
-forma.faces.push(  new THREE.Face3(1,2,3) );
-forma.faces.push(  new THREE.Face3(1,3,4) );
-forma.faces.push(  new THREE.Face3(4,5,1) );
-forma.faces.push(  new THREE.Face3(6,5,1) );
-forma.faces.push(  new THREE.Face3(6,5,7) );
-forma.faces.push(  new THREE.Face3(5,8,7) );
-forma.faces.push(  new THREE.Face3(7,2,8) );
-forma.faces.push(  new THREE.Face3(8,3,2) );
-forma.faces.push(  new THREE.Face3(1,2,6) );
-forma.faces.push(  new THREE.Face3(2,6,7) );
-forma.faces.push(  new THREE.Face3(3,4,5) );
-forma.faces.push(  new THREE.Face3(5,3,8) );
+forma.faces.push(  new THREE.Face3(0,1,2) );
+forma.faces.push(  new THREE.Face3(0,2,3) );
+forma.faces.push(  new THREE.Face3(3,4,0) );
+forma.faces.push(  new THREE.Face3(5,4,0) );
+forma.faces.push(  new THREE.Face3(5,4,6) );
+forma.faces.push(  new THREE.Face3(4,7,6) );
+forma.faces.push(  new THREE.Face3(6,1,7) );
+forma.faces.push(  new THREE.Face3(7,2,1) );
+forma.faces.push(  new THREE.Face3(0,1,5) );
+forma.faces.push(  new THREE.Face3(1,5,6) );
+forma.faces.push(  new THREE.Face3(2,3,4) );
+forma.faces.push(  new THREE.Face3(4,2,7) );
 
 //Se solicita el computo de la esfera de menor tamaño que acota al objeto
 forma.computeBoundingSphere();
