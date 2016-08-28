6875a923a561d11637aa3805bb872b44d0ab4e8c
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-27 19:13:38 -0500

    Update Ejercicio3-cubo.js

--- rv/Ejercicio3-cubo.js
+++ rv/Ejercicio3-cubo.js
@@ -1,1 +1,17 @@
 
+var estrella=new THREE.Geomtetry();
+//Coordenadas de los vértices
+forma.vertices.push(  new THREE.Vector3(1,1,1) );
+forma.vertices.push(  new THREE.Vector3(1,1,-1) );
+forma.vertices.push(  new THREE.Vector3(-1,1,-1) );
+forma.vertices.push(  new THREE.Vector3(-1,1,1) );
+forma.vertices.push(  new THREE.Vector3(-1,-1,1) );
+forma.vertices.push(  new THREE.Vector3(1,-1,1) );
+forma.vertices.push(  new THREE.Vector3(1,-1,-1) );
+forma.vertices.push(  new THREE.Vector3(-1,-1,-1) );
+
+//Contruyendo las caras con los vértices
+forma.face.push(  new THREE.Face3() );
+forma.face.push(  new THREE.Face3() );
+forma.face.push(  new THREE.Face3() );
+forma.face.push(  new THREE.Face3() );
