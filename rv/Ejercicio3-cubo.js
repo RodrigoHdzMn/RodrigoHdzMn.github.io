3ba6d12baf34e199f895be39ebecab61d0fbf57a
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-28 13:34:18 -0500

    Update Ejercicio3-cubo.js

--- rv/Ejercicio3-cubo.js
+++ rv/Ejercicio3-cubo.js
@@ -11,8 +11,8 @@ forma.vertices.push(  new THREE.Vector3(1,-1,-1) );
 forma.vertices.push(  new THREE.Vector3(-1,-1,-1) );
 
 //Contruyendo las caras con los vértices
-//forma.faces.push(  new THREE.Face3(0,1,2) );
-//forma.faces.push(  new THREE.Face3(0,2,3) );
+forma.faces.push(  new THREE.Face3(0,1,2) );
+forma.faces.push(  new THREE.Face3(0,2,3) );
 //forma.faces.push(  new THREE.Face3(0,3,4) );
 //forma.faces.push(  new THREE.Face3(0,4,5) );
 //forma.faces.push(  new THREE.Face3(0,5,6) );
