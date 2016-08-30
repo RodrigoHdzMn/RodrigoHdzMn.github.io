3aae7889a9f4934ee3999a321ab24752dcc896d2
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-08-30 15:47:57 -0500

    Update Ejercicio5-Torre.js

--- rv/Ejercicio5-Torre.js
+++ rv/Ejercicio5-Torre.js
@@ -1,5 +1,5 @@
 
-var baseForma= new THREE.CylinderGeometry(.02, .1, 0.3 );
+var baseForma= new THREE.CylinderGeometry(.02, .4, 0.3 );
 var esferaForma= new THREE.SphereGeometry(.65);
 esferaForma.translate(0,1,0);
 
