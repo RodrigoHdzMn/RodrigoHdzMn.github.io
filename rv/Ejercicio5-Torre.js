9522c30d29db1a4956452b879dce2e95f91e37b4
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-01 15:03:43 -0500

    Update Ejercicio5-Torre.js

--- rv/Ejercicio5-Torre.js
+++ rv/Ejercicio5-Torre.js
@@ -6,16 +6,19 @@ esferaForma.translate(0,1,0);
 var baseMalla=new THREE.Mesh(baseForma);
 var esferaMalla=new THREE.Mesh(esferaForma);
 
-var arbolForma=new THREE.Geometry();
+var torreForma=new THREE.Geometry();
 
-arbolForma.merge(baseMalla.geometry, baseMalla.matrix);
-arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
+torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+torreForma.merge(esferaMalla.geometry, esferaMalla.matrix);
 
 var material=new THREE.MeshNormalMaterial();
-var arbolMalla= new THREE.Mesh(arbolForma,material);
+var torreMalla= new THREE.Mesh(torreForma,material);
+
+torreMalla.rotateX(Math.PI/4);
+torreMalla.rotateY(Math.PI/4);
 
 var escena=new THREE.Scene();
-escena.add(arbolMalla);
+escena.add(torreMalla);
 
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
