b4a8f6c094632ae4ecdecf2e97fdddb5750f739b
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-01 15:30:42 -0500

    Update Ejercicio5-Torre.js

--- rv/Ejercicio5-Torre.js
+++ rv/Ejercicio5-Torre.js
@@ -2,7 +2,7 @@ var points = [];
 for ( var i = 0; i < 10; i ++ ) {
 	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
 }
-var geometry = new THREE.LatheGeometry( points );
+var geometry = new THREE.LatheGeometry( points,60 );
 var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 var lathe = new THREE.Mesh( geometry, material );
 scene.add( lathe );
