9eb79b15c069e79800d86fdb63c528cb4fd9f1d7
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-01 15:44:38 -0500

    Update Ejercicio5-Torre.js

--- rv/Ejercicio5-Torre.js
+++ rv/Ejercicio5-Torre.js
@@ -10,7 +10,7 @@ var escena=new THREE.Scene();
 escena.add(lathe);
 
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=10;
+camara.position.z=5;
 
 renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95 );
