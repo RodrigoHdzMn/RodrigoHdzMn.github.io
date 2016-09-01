6eac77c463127bbeb60ac46b3bfd59627a471bf3
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-01 15:31:38 -0500

    Update Ejercicio5-Torre.js

--- rv/Ejercicio5-Torre.js
+++ rv/Ejercicio5-Torre.js
@@ -6,3 +6,11 @@ var geometry = new THREE.LatheGeometry( points,60 );
 var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 var lathe = new THREE.Mesh( geometry, material );
 scene.add( lathe );
+
+var camara=new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+renderizador=new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95 );
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
