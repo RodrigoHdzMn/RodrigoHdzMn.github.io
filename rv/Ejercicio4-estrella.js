b1d7b761b4c5eb7dfe8678d4e6fc0dd7b7637be8
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-02 18:07:12 -0500

    Update Ejercicio4-estrella.js

--- rv/Ejercicio4-estrella.js
+++ rv/Ejercicio4-estrella.js
@@ -5,7 +5,7 @@ var figura =new THREE.Shape();
 figura.moveTo(20,10);
 figura.lineTo(60,100);
 figura.lineTo(100,10);
-figura.lineTo(40,50);
+figura.lineTo(60,35);
 //figura.lineTo();
 //figura.lineTo();
 
