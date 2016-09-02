8f8243231a5c9a6a757fa00b3621b1c0ce7eb7a0
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-02 17:05:35 -0500

    Update Ejercicio4-estrella.js

--- rv/Ejercicio4-estrella.js
+++ rv/Ejercicio4-estrella.js
@@ -19,7 +19,7 @@ var camara= new THREE.PerspectiveCamera();
 camara.position.z=200;
 
 var renderizador=new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+renderizador.setSize(window.innerHeight*.75,window.innerHeight*.75);
 
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
