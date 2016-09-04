6c818b7fd1c07826da756c8f69225d7e55c8c7ed
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-03 19:55:55 -0500

    Update Ejercicio5-Torre.js

--- rv/Ejercicio5-Torre.js
+++ rv/Ejercicio5-Torre.js
@@ -26,7 +26,7 @@ torreForma.merge(forma4Malla.geometry, forma4Malla.matrix);
 var material=new THREE.MeshNormalMaterial();
 var torreMalla= new THREE.Mesh(torreForma,material);
 
-torreMalla.rotateX(Math.PI/8);
+//torreMalla.rotateX(Math.PI/8);
 
 var escena=new THREE.Scene();
 escena.add(torreMalla);
