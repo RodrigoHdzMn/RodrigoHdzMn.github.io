f77a345de85421f6b511ec653885c36a36f576ed
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-03 20:12:17 -0500

    Update Ejercicio5-Torre.js

--- rv/Ejercicio5-Torre.js
+++ rv/Ejercicio5-Torre.js
@@ -8,7 +8,7 @@ forma2.translate(0,-0.8,0);
 var forma3 = new THREE.CylinderGeometry(.4, .6, 0.2,60 );
 forma3.translate(0,-0.6,0);
 
-var forma4 = new THREE.CylinderGeometry(.4, .4, 1.5,60 );
+var forma4 = new THREE.CylinderGeometry(.4, .4, 2,60 );
 //forma4.translate(0,-0.15,0);
 
 var baseMalla=new THREE.Mesh(baseForma);
