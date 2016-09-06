db3a973c92728214df93a1150b4fc526fce2ee0f
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-06 15:02:48 -0500

    Update ejemplo-camaradeproyeccion.js

--- rv/ejemplo-camaradeproyeccion.js
+++ rv/ejemplo-camaradeproyeccion.js
@@ -5,7 +5,7 @@ var relacionAspecto=window.innerWidth/window.innerHeight;
 var planoCercano=1;
 var planoLejano=1000;
 
-var camara=new THREE.PerpectiveCamera(campoVision,relacionAspecto;planoCercano;planoLejano);
+var camara=new THREE.PerpectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
 
 camara.position.z=15;
 
