cd6ad3160782b5f413b0794ad7dc68c5c6bcc414
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-13 15:00:55 -0500

    Create ejemplo-LambertMaterial.js

---
+++ rv/ejemplo-LambertMaterial.js
@@ -0,0 +1,16 @@
+
+var forma = new THREE.SphereGeometry(1);
+var material=new THREE.MeshLambertMaterial({color:0x00cc00});
+var malla= new THREE.Mesh(forma,material);
+
+var escena= new THREE.Scene();
+escena.add(malla);
+
+var camara= new THREE.PerpectiveCamera();
+camera.position.z=5;
+
+var lienzo = document.getElementById("lambertMaterial");
+var renderizador=new THREE.WebGLRender({canvas:lienzo, antialias: true});
+
+renderizador.setSize(600,600);
+renderizador.render(escena,camara);
