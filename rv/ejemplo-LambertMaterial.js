c5554e0df88b91a740017acf11ec3c6a3dca028e
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-13 15:06:55 -0500

    Update ejemplo-LambertMaterial.js

--- rv/ejemplo-LambertMaterial.js
+++ rv/ejemplo-LambertMaterial.js
@@ -8,9 +8,3 @@ escena.add(malla);
 
 var camara= new THREE.PerspectiveCamera();
 camera.position.z=5;
-
-var lienzo = document.getElementById("lambertMaterial");
-var renderizador=new THREE.WebGLRender({canvas:lienzo, antialias: true});
-
-renderizador.setSize(600,600);
-renderizador.render(escena,camara);
