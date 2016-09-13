a101a7be62253534d21fb0f4daafb5395e0707dc
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-13 15:36:01 -0500

    Update ejemplo-sombras.js

--- rv/ejemplo-sombras.js
+++ rv/ejemplo-sombras.js
@@ -19,7 +19,7 @@ var camara= new THREE.PerspectiveCamera();
 camara.position.z=15;
 camara.position.y=5;
 
-var lienzo = document.getElementById("luzAmbiental");
+var lienzo = document.getElementById("luzSinSombras");
 var renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias: true});
 
 renderizador.setSize(600,600);
