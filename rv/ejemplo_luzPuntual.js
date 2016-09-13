f954f39d46ae89724af7fcb31ee5dbbdc0fc642d
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-13 15:15:44 -0500

    Update ejemplo_luzPuntual.js

--- rv/ejemplo_luzPuntual.js
+++ rv/ejemplo_luzPuntual.js
@@ -23,7 +23,7 @@ var camara= new THREE.PerpectiveCamera();
 camara.position.z=5;
 
 var lienzo = document.getElementById("basicMaterial");
-var renderizador=new THREE.WebGLRender({canvas:lienzo, antialias: true});
+var renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias: true});
 
 renderizador.setSize(600,600);
 renderizador.render(escena,camara);
