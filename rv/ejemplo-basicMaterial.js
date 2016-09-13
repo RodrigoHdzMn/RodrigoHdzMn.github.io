123bc4dca5f217325e5b0e748f333d4920c90f27
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-13 15:15:30 -0500

    Update ejemplo-basicMaterial.js

--- rv/ejemplo-basicMaterial.js
+++ rv/ejemplo-basicMaterial.js
@@ -10,7 +10,7 @@ var camara= new THREE.PerspectiveCamera();
 camara.position.z=5;
 
 var lienzo = document.getElementById("basicMaterial");
-var renderizador=new THREE.WebGLRender({canvas:lienzo, antialias: true});
+var renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias: true});
 
 renderizador.setSize(600,600);
 renderizador.render(escena,camara);
