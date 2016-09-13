6f754427081237f723664932e8afd1041e3822e6
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-13 15:09:11 -0500

    Create ejemplo_luzPuntual.js

---
+++ rv/ejemplo_luzPuntual.js
@@ -0,0 +1,29 @@
+
+
+var lienzo = document.getElementById("lambertMaterial");
+var renderizador=new THREE.WebGLRender({canvas:lienzo, antialias: true});
+
+renderizador.setSize(600,600);
+renderizador.render(escena,camara);
+
+var luzPuntual= new THREE.PointLight(0xFFFFFF);
+luzPuntual.position.x=10;
+luzPuntual.position.y=10;
+luzPuntual.position.z=10;
+
+var forma = new THREE.SphereGeometry(1);
+var material=new THREE.MeshBasicMaterial({color:0x00ff00});
+var malla= new THREE.Mesh(forma,material);
+
+var escena= new THREE.Scene();
+escena.add(malla);
+escena.add(luzPuntual);
+
+var camara= new THREE.PerpectiveCamera();
+camara.position.z=5;
+
+var lienzo = document.getById("basicMaterial");
+var renderizador=new THREE.WebGLRender({canvas:lienzo, antialias: true});
+
+renderizador.setSize(600,600);
+renderizador.render(escena,camara);
