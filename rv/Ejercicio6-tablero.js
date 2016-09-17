1eb4908ba6b87c9f3a62c7cd82f24fdc6483a051
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-16 23:39:38 -0500

    Update Ejercicio6-tablero.js

--- rv/Ejercicio6-tablero.js
+++ rv/Ejercicio6-tablero.js
@@ -125,8 +125,8 @@ torreForma.merge(formaA4Malla.geometry,formaA4Malla.matrix);
 torreForma.merge(formaA5Malla.geometry,formaA5Malla.matrix);
 torreForma.merge(formaA6Malla.geometry,formaA6Malla.matrix);
 
-var materialN1=new THREE.MeshNormalMaterial( {color: 0xffffff} );
-var materialN2=new THREE.MeshNormalMaterial( {color: 0x727272});
+var materialN1=new THREE.MeshBasicMaterial( {color: 0xffffff} );
+var materialN2=new THREE.MeshBasicMaterial( {color: 0x727272});
 
 var torreMalla1= new THREE.Mesh(torreForma,materialN1);
 
