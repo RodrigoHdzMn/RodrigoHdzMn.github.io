a4917aaf6f97bbc17fd9567fac9f8b59afad5eda
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-16 21:18:55 -0500

    Update Ejercicio6-tablero.js

--- rv/Ejercicio6-tablero.js
+++ rv/Ejercicio6-tablero.js
@@ -8,7 +8,7 @@ var planoLejano=1000;
 
 var camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
 camara.position.z = 200;
-camara.position.y = 100;
+camara.position.y = -50;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
