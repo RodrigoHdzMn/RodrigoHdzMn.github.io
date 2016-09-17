8b568e3c7c8aaa26620c055ff4035f202ea9005e
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-16 20:49:16 -0500

    Update Ejercicio6-tablero.js

--- rv/Ejercicio6-tablero.js
+++ rv/Ejercicio6-tablero.js
@@ -13,15 +13,30 @@ document.body.appendChild (renderizador.domElement);
 
 for(var i=1; i<=8; i++)
 {
+   for(var j=1; j<=8; j++)
+   {
 	var forma = new THREE.BoxGeometry( 10,10,1);
+	
+	if(j%2 ==0)
+	{
 		if(i%2 == 0)
 		var material = new THREE.MeshBasicMaterial( {color: 0xffffff}); 
 		else
 		var material = new THREE.MeshBasicMaterial( {color: 0x727272}); //0xcfcfcf
+	}
+	else
+	{
+		if(i%2 == 0)
+		var material = new THREE.MeshBasicMaterial( {color: 0xffffff}); 
+		else
+		var material = new THREE.MeshBasicMaterial( {color: 0x727272}); //0xcfcfcf
+		
+	}
 	
 	var cubo = new THREE.Mesh(forma, material);
 	cubo.position.x=10*i;
 	escena.add(cubo);
+   }
 }
 
 
