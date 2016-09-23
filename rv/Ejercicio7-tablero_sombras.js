a3b75e78346c400fc8a80f80739f7b64fcf6e139
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-23 09:47:40 -0500

    Update Ejercicio7-tablero_sombras.js

--- rv/Ejercicio7-tablero_sombras.js
+++ rv/Ejercicio7-tablero_sombras.js
@@ -7,7 +7,7 @@ var planoCercano=1;
 var planoLejano=1000;
 
 var camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
-camara.position.z = 250;
+camara.position.z = 280;
 camara.position.y = 30;
 camara.position.x = 50;
 
