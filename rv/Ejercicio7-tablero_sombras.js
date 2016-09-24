302adce2b9397a716a07fdbcc72718fb9967c87d
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-23 19:28:06 -0500

    Update Ejercicio7-tablero_sombras.js

--- rv/Ejercicio7-tablero_sombras.js
+++ rv/Ejercicio7-tablero_sombras.js
@@ -148,8 +148,8 @@ torreForma.merge(formaA4Malla.geometry,formaA4Malla.matrix);
 torreForma.merge(formaA5Malla.geometry,formaA5Malla.matrix);
 torreForma.merge(formaA6Malla.geometry,formaA6Malla.matrix);
 
-var materialN1=new THREE.MeshBasicMaterial( {color: 0xffe100} );
-var materialN2=new THREE.MeshBasicMaterial( {color: 0x000ef5});
+var materialN1=new THREE.MeshLambertMaterial( {color: 0xffe100} );
+var materialN2=new THREE.MeshLambertMaterial( {color: 0x000ef5});
 
 var torreMalla1= new THREE.Mesh(torreForma,materialN1);
 torreMalla1.position.x=10;
