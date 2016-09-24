bc49f4d5b3a90766257c88a6162af21ee71f5700
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-23 21:36:19 -0500

    Update Ejercicio7-tablero_sombras.js

--- rv/Ejercicio7-tablero_sombras.js
+++ rv/Ejercicio7-tablero_sombras.js
@@ -54,7 +54,7 @@ for(var i=1; i<=8; i++)
 	var cubo = new THREE.Mesh(forma, material);
 	cubo.position.x=10*i;
 	cubo.position.z=10*j;
-	//escena.add(cubo);
+	escena.add(cubo);
 	
    }
 }
@@ -174,7 +174,7 @@ escena.add(torreMalla2);
 escena.add(torreMalla3);
 escena.add(torreMalla4);
 //Fin Torre
-escena.add(cubo);
+
 //escena.add(LuzMagenta)
 //escena.add(LuzCyan)
 escena.add(LuzYellow)
