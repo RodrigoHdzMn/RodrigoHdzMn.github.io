0aedba905724fc1ca24523b353ac5f3b57b04936
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-09-27 15:41:47 -0500

    Update funcion-cargarTextura.js

--- rv/funcion-cargarTextura.js
+++ rv/funcion-cargarTextura.js
@@ -6,7 +6,7 @@ TEXTURA.retrollamada = function(textura)
 {
       var material= new THREE.MeshBasicMaterial( {map:textura} );
       TEXTURA.malla= new THREE.Mesh(new THREE.SphereGeometry(1), material);
-      TEXTURA.escena.add(TEXTURA,malla);
+      TEXTURA.escena.add(TEXTURA.malla);
 }
 
 TEXTURA.setup=function()
@@ -18,7 +18,7 @@ TEXTURA.setup=function()
       
       TEXTURA.camara= new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
       
-      VENTANA.camara.position.z=5;
+      TEXTURA.camara.position.z=5;
       
       var lienzo=document.getElementById("ejemplo-textura");
       TEXTURA.renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias: true});
