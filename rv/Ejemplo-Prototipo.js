346bc2fe7c95dc8635f85b4c4acd9de58201eed6
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-04 15:52:53 -0500

    Create Ejemplo-Prototipo.js

---
+++ rv/Ejemplo-Prototipo.js
@@ -0,0 +1,51 @@
+
+var PROTOTIPO = new Object();
+
+PROTOTIPO.ArbolGeometry = function()
+{
+      var troncoForma = new THREE.CylinderGeometry(-.25,.5,1);
+      var esferaForma = new THREE.SphereGeometry(0.65);
+      esferaForma.translate(0,1,0);
+      
+      var troncoMalla = new THREE.Mesh(troncoForma);
+      var esferaMalla = new THREE.Mesh(esferaForma);
+      
+      this.merge(troncoMalla.geometry, troncoMalla.matrix);
+      this.merge(esferaMalla.geometry, esferaMalla.matrix);
+      
+      var material = new THREE.MeshNormalMaterial();
+      this.malla = new THREE.Mesh(arbolForma,material);
+}
+
+PROTOTIPO.ArbolGeometry.prototype = new THREE.Geometry();
+
+PROTOTIPO.setup = function()
+{
+      var arbol1= new Arbol( new PROTOTIPO.ArbolGeometry(),
+                             new PROTOTIPO.MeshNormalMaterial());
+      var arbol2= new Arbol( new PROTOTIPO.ArbolGeometry(),
+                             new PROTOTIPO.MeshNormalMaterial());
+      
+      arbol1.malla.position.x = -5;
+      arbol2.malla.position.x =  5;
+      
+      PROTOTIPO.camara = new THREE.PerspectiveCamera();
+      PROTOTIPO.camara.position.z = 20;
+      
+      var lienzo = document.getElementById("ejemplo-prototipo");
+      PROTOTIPO.renderizador = new THREE.WebGLRenderer({canvas:lienzo, antialias:true});
+      PROTOTIPO.renderizador.setSize(600,600);
+      
+      PROTOTIPO.escena = new THREE.Scene();
+      PROTOTIPO.escena.add(arbol1.malla);
+      PROTOTIPO.escena.add(arbol2.malla);
+}
+
+PROTOTIPO.loop = function()
+{
+      requestAnimationFrame( PROTOTIPO.loop );
+      CONSTRUCTOR.renderizador.render( PROTOTIPO.escena, PROTOTIPO.camara );
+}
+
+PROTOTIPO.setup();
+PROTOTIPO.loop();
