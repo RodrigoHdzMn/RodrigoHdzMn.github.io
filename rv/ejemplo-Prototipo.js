347ba8cde5ba1be321083f4e3972adaacaae7858
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-04 21:39:04 -0500

    Update ejemplo-Prototipo.js

--- rv/ejemplo-Prototipo.js
+++ rv/ejemplo-Prototipo.js
@@ -4,6 +4,7 @@ var PROTOTIPO = new Object();
 PROTOTIPO.ArbolGeometry = function()
 {
       THREE.Geometry.call(this);
+      
       var troncoForma = new THREE.CylinderGeometry(-.25,.5,1);
       var esferaForma = new THREE.SphereGeometry(0.65);
       esferaForma.translate(0,1,0);
