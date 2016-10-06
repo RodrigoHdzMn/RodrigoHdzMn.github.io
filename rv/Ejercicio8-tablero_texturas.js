ab6330d05c967c0d27de5912edb3eb2f1cf739f6
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-06 11:38:04 -0500

    Update Ejercicio8-tablero_texturas.js

--- rv/Ejercicio8-tablero_texturas.js
+++ rv/Ejercicio8-tablero_texturas.js
@@ -3,36 +3,60 @@ var TEXTURA=new Object();
 TEXTURA.retrollamada = function(textura)
 {
       var material= new THREE.MeshBasicMaterial( {map:textura} );
-      TEXTURA.malla= new THREE.Mesh(new THREE.SphereGeometry(1), material);
-      TEXTURA.escena.add(TEXTURA.malla);
+      var forma = new THREE.BoxGeometry( 10,.1,10);
+      TEXTURA.mallaCubo= new THREE.Mesh(forma,material);
 }
 
 TEXTURA.setup=function()
 {
       TEXTURA.escena = new THREE.Scene();
       
+      var campoVision=30;// grado
+      var relacionAspecto=window.innerWidth/window.innerHeight;
+      var planoCercano=1;
+      var planoLejano=1000;
+      
+      TEXTURA.camara=new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
+      TEXTURA.camara.position.z = 200;
+      TEXTURA.camara.position.y = 25;
+      TEXTURA.camara.position.x = 50;
+      
       var cargador = new THREE.TextureLoader();
-      cargador.load("Textura_marmol_blanco.jpg",TEXTURA.retrollamada);
-      
-      TEXTURA.camara= new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
-      
-      TEXTURA.camara.position.z=5;
+
+      for(var i=1; i<=8; i++)
+      {
+            for(var j=1; j<=8; j++)
+            {
+	            if(j%2 ==0)
+	            {
+		            if(i%2 == 0)
+		            cargador.load("Textura_marmol_negro.jpg",TEXTURA.retrollamada); 
+		            else
+		            cargador.load("Textura_marmol_blanco.jpg",TEXTURA.retrollamada);
+	            }
+	            else
+	            {
+		            if(i%2 == 0)
+		            cargador.load("Textura_marmol_blanco.jpg",TEXTURA.retrollamada);
+		      else
+		           cargador.load("Textura_marmol_negro.jpg",TEXTURA.retrollamada);
+	            }
+	
+	            TEXTURA.mallaCubo.position.x=10*i;
+	            TEXTURA.mallaCubo.position.z=10*j;
+	            TEXTURA.escena.add(TEXTURA.mallaCubo);
+            }
+      }
       
       var lienzo=document.getElementById("tablero_texturas");
       TEXTURA.renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias: true});
       TEXTURA.renderizador.setSize(600,600);
 }
 
-TEXTURA.loop=function()
+TEXTURA.main=function()
 {
-      requestAnimationFrame(TEXTURA.loop);
-      if(TEXTURA.malla !== undefined)
-      {
-            TEXTURA.malla.rotateX( 0.01 );
-            TEXTURA.malla.rotateY( 0.01 );
-      }
       TEXTURA.renderizador.render(TEXTURA.escena,TEXTURA.camara)
 }
 
 TEXTURA.setup();
-TEXTURA.loop();
+TEXTURA.main();
