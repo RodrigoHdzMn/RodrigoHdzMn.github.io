f010f8a06ee1a9377f10117ac365ba8e12514616
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-05 23:42:06 -0500

    Update Ejercicio8-tablero_texturas.js

--- rv/Ejercicio8-tablero_texturas.js
+++ rv/Ejercicio8-tablero_texturas.js
@@ -19,8 +19,7 @@ TEXTURA.setup=function()
       
       TEXTURA.camara.position.z=5;
       
-      var lienzo=document.getElementById("ejemplo-textura");
-      TEXTURA.renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias: true});
+      TEXTURA.renderizador=new THREE.WebGLRenderer();
       TEXTURA.renderizador.setSize(600,600);
 }
 
