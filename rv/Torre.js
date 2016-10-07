016e7c6846e08768d616d62bf609d8c9f9b67984
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-07 09:52:41 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -30,8 +30,8 @@ forma5.rotateX(Math.PI/2);
 forma5.translate(0,Tb*1.65,0);
 
 var altura6=altura5;
-var forma6 = new THREE.SphereGeometry( 0.8, 32, 32 );
-forma6.translate(0,Tb*2.2,0);
+var forma6 = new THREE.SphereGeometry( 0.9, 32, 32 );
+forma6.translate(0,Tb*2.3,0);
 
 var altura7=Tb/8;
 var forma7 = new THREE.CylinderGeometry(Tb*3/4, Tb*9/16, altura7,60 );
