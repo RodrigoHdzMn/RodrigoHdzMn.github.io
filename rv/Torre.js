49593f5f1c463d3c05403e714355e67f1f896806
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-06 23:13:04 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -120,7 +120,7 @@ escena.add(base);
 escena.add(lathe);
 
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=15;
+camara.position.z=150;
 
 renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.75,window.innerHeight*.75 );
