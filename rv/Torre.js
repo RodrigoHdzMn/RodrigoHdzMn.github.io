360d84ed1889a2492ffd83d184b67442de91eff3
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-08 13:38:44 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -19,7 +19,7 @@ for ( var i = 3; i < 18; i ++ )
 {
 	var a= Math.cos( i * 0.2 ) * 5 + 10 ;
 	var b=( i - 5 ) * 2;
-	points.push( new THREE.Vector2( 0.75*a, 0.75*b ) );//new THREE.Vector2( 0.06*a, 0.1*b )
+	points.push( new THREE.Vector2( 0.1*a, 0.1*b ) );//new THREE.Vector2( 0.06*a, 0.1*b )
 	
 }
 var forma4 = new THREE.LatheGeometry( points );
