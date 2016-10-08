0883a42aad42f326c9fd556f27c4c01315395a51
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-07 22:51:37 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -5,7 +5,7 @@ for ( var i = 0; i < 10; i ++ ) {
 	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
 }
 var geometry = new THREE.LatheGeometry( points );
-var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+var material = new THREE.MeshBasicMaterial(  );
 var lathe = new THREE.Mesh( geometry, material );
 escena.add( lathe );
 
