8b0a0c8e7a9f25062a2238939a5f8ad372935de6
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-08 16:45:51 -0500

    Update Torre.js

--- rv/Torre.js
+++ rv/Torre.js
@@ -23,9 +23,9 @@ var Tb=0.8*2;//tamaño base
 	var forma4 = new THREE.LatheGeometry( points ,64);
 	forma4.translate(0,1.7,0);
 
-	var forma5 = new THREE.TorusGeometry( 0.6, 0.2, 10, 100 );
+	var forma5 = new THREE.TorusGeometry( 0.6, 0.2, 8, 100 );
 	forma5.rotateX(Math.PI/2);
-	forma5.translate(0,4.6,0);
+	forma5.translate(0,4.65,0);
 
 	var baseMalla=new THREE.Mesh(baseForma);
 	var base2Malla=new THREE.Mesh(baseForma2);
