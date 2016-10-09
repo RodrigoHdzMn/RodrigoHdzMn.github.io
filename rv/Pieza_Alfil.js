c5e6b35c279efc600306780a087751cfc2af7db8
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-08 21:25:32 -0500

    Update Pieza_Alfil.js

--- rv/Pieza_Alfil.js
+++ rv/Pieza_Alfil.js
@@ -4,48 +4,48 @@ var Tb=0.8*2;//tamaño base
         var baseForma= new THREE.CylinderGeometry(Tb, Tb*1.25, altura1,60 );//dia.sup, dia.inf, altura
         baseForma.translate(0,0,0);
 	
-        var baseForma2= new THREE.TorusGeometry( 1, 0.8, 15, 100 );
+        var baseForma2= new THREE.TorusGeometry( Tb*5/8, Tb/2, Tb*75/8, Tb*62.5 );
         baseForma2.rotateX(Math.PI/2);
-        baseForma2.translate(0,0.72,0);
+        baseForma2.translate(0,Tb*9/20,0);
 
-        var forma3 = new THREE.TorusGeometry( 1.4, 0.2, 15, 100 );
+        var forma3 = new THREE.TorusGeometry( Tb*7/8, Tb/8, Tb*75/8, Tb*62.5 );
         forma3.rotateX(Math.PI/2);
-        forma3.translate(0,1.3,0);
+        forma3.translate(0,Tb*13/16,0);
 	
         var points = [];
 	for ( var i = 4; i < 20; i ++ )
 	{
 		  var a= Math.cos( i * 0.2 ) * 5 + 10 ;
 		  var b=( i - 5 ) * 2;
-		  points.push( new THREE.Vector2( 0.1*a, 0.1*b ) );//new THREE.Vector2( 0.06*a, 0.1*b )
+		  points.push( new THREE.Vector2( (Tb/16)*a, (Tb/16)*b ) );
 	}
 	var forma4 = new THREE.LatheGeometry( points ,64);
-	forma4.translate(0,1.7,0);
+	forma4.translate(0,Tb*17/16,0);
 
-	var forma5 = new THREE.TorusGeometry( 0.6, 0.2, 6, 100 );
+	var forma5 = new THREE.TorusGeometry( Tb*3/8, Tb/8, Tb*15/4, Tb*62.5 );
 	forma5.rotateX(Math.PI/2);
-        forma5.translate(0,4.7,0);
+        forma5.translate(0,Tb*47/16,0);
 
-	var forma6 = new THREE.TorusGeometry( 0.4, 0.2, 20, 100 );
+	var forma6 = new THREE.TorusGeometry( Tb/4, Tb/8, Tb*12.5, Tb*62.5 );
 	forma6.rotateX(Math.PI/2);
-	forma6.translate(0,4.9,0);
+	forma6.translate(0,Tb*49/16,0);
 
-	var forma7 = new THREE.TorusGeometry( 0.4, 0.13, 20, 100 );
+	var forma7 = new THREE.TorusGeometry( Tb/4, Tb*13/160, Tb*12.5, Tb*62.5 );
 	forma7.rotateX(Math.PI/2);
-        forma7.translate(0,5.2,0);
+        forma7.translate(0,Tb*13/4,0);
 
 	var points2 = [];
 	for ( var i = -4; i < 7; i ++ )
 	{
 		  var a= Math.cos( i * 0.4 ) * 6 + 5.5 ;
 		  var b=( i - 5 ) * 2;
-		   points2.push( new THREE.Vector2( 0.06*a, 0.06*b ) );//new THREE.Vector2( 0.06*a, 0.1*b )
+		   points2.push( new THREE.Vector2( (Tb*3/80)*a, (tb*3/80)*b ) );//new THREE.Vector2( 0.06*a, 0.1*b )
 	}
 	var forma8 = new THREE.LatheGeometry( points2 ,64);
-	forma8.translate(0,6.3,0);
+	forma8.translate(0,Tb*63/16,0);
 
-	var forma9 = new THREE.SphereGeometry( 0.2, 32, 32 );
-	forma9.translate(0,6.5,0);
+	var forma9 = new THREE.SphereGeometry( Tb/8, Tb*20, Tb*20 );
+	forma9.translate(0,Tb*65/16,0);
 
   	var baseMalla=new THREE.Mesh(baseForma);
 	var base2Malla=new THREE.Mesh(baseForma2);
