67b368bde2bf35ca20f9087db1721bcddc578fcb
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-09 02:50:53 -0500

    Update Ejercicio7-tablero_sombras.js

--- rv/Ejercicio7-tablero_sombras.js
+++ rv/Ejercicio7-tablero_sombras.js
@@ -192,8 +192,8 @@ torreMalla4.castShadow=true;
 LuzMagenta.castShadow=true;
 LuzCyan.castShadow=true;
 LuzYellow.castShadow=true;
-//for ( var n=1; n<=64; n ++)
-//tablero[n].receiveShadow = true;
+for ( var n=0; n<64; n ++)
+tablero[n].receiveShadow = true;
 
 renderizador.render(escena,camara);
 
