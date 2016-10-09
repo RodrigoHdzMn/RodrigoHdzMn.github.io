d6fca00e0ca8a7f9c5344ee1afba65100bbd0e44
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-09 13:25:50 -0500

    Update Ejercicio7-tablero_sombras.js

--- rv/Ejercicio7-tablero_sombras.js
+++ rv/Ejercicio7-tablero_sombras.js
@@ -1,7 +1,7 @@
 
 var escena = new THREE.Scene();
 
-var LuzMagenta=new THREE.PointLight(0xffffff); //new THREE.PointLight(0xff00ff);
+var LuzMagenta=new THREE.PointLight(0xff00ff);
 LuzMagenta.position.y=40;
 LuzMagenta.position.x=40;
 LuzMagenta.position.z=40;
