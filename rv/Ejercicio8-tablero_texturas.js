7718bde2abd7ac94a34cd6f07dbaed3f2baf5cdf
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-15 01:37:52 -0500

    Update Ejercicio8-tablero_texturas.js

--- rv/Ejercicio8-tablero_texturas.js
+++ rv/Ejercicio8-tablero_texturas.js
@@ -1,4 +1,42 @@
-var i,j;
+for(var i=1; i<=8; i++)
+{
+   for(var j=1; j<=8; j++)
+   {
+	var forma = new THREE.BoxGeometry( 10,.1,10);
+	
+	if(j%2 ==0)
+	{
+		if(i%2 == 0){
+		var material = new THREE.MeshBasicMaterial( {color: 0xffffff});
+		var cubo = new THREE.Mesh(forma, material);
+		cubo.position.x=10*i;
+		cubo.position.z=10*j;
+		escena.add(cubo);}
+		else{
+		var material = new THREE.MeshBasicMaterial( {color: 0x727272}); //0xcfcfcf
+		var cubo = new THREE.Mesh(forma, material);
+		cubo.position.x=10*i;
+		cubo.position.z=10*j;
+		escena.add(cubo);}
+	}
+	else
+	{
+		if(i%2 == 0){
+		var material = new THREE.MeshBasicMaterial( {color: 0x727272}); //0xcfcfcf
+		var cubo = new THREE.Mesh(forma, material);
+		cubo.position.x=10*i;
+		cubo.position.z=10*j;
+		escena.add(cubo);}
+		else{
+		var material = new THREE.MeshBasicMaterial( {color: 0xffffff}); //0xcfcfcf
+		var cubo = new THREE.Mesh(forma, material);
+		cubo.position.x=10*i;
+		cubo.position.z=10*j;
+		escena.add(cubo);}
+	}
+   }
+}
+/*var i,j;
 for(i=1; i<=8; i++)
 {
    for(j=1; j<=8; j++)
@@ -70,7 +108,7 @@ for(i=1; i<=8; i++)
 		}
 	}
    }
-}
+}*/
 
 function Torre(posX,posZ,materialTextura)
 {
