5d7eeb10be16b9b487db68b6a93b6cec7827d067
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-15 01:44:33 -0500

    Update Ejercicio8-tablero_texturas.js

--- rv/Ejercicio8-tablero_texturas.js
+++ rv/Ejercicio8-tablero_texturas.js
@@ -1,18 +1,18 @@
 for(var i=1; i<=8; i++)
 {
    for(var j=1; j<=8; j++)
-   {
-	var forma = new THREE.BoxGeometry( 10,.1,10);
-	
+   {	
 	if(j%2 ==0)
 	{
 		if(i%2 == 0){
+		var forma = new THREE.BoxGeometry( 10,.1,10);
 		var material = new THREE.MeshBasicMaterial( {color: 0xffffff});
 		var cubo = new THREE.Mesh(forma, material);
 		cubo.position.x=10*i;
 		cubo.position.z=10*j;
 		grupo.add(cubo);}
 		else{
+		var forma = new THREE.BoxGeometry( 10,.1,10);
 		var material = new THREE.MeshBasicMaterial( {color: 0x727272}); //0xcfcfcf
 		var cubo = new THREE.Mesh(forma, material);
 		cubo.position.x=10*i;
@@ -22,12 +22,14 @@ for(var i=1; i<=8; i++)
 	else
 	{
 		if(i%2 == 0){
+		var forma = new THREE.BoxGeometry( 10,.1,10);
 		var material = new THREE.MeshBasicMaterial( {color: 0x727272}); //0xcfcfcf
 		var cubo = new THREE.Mesh(forma, material);
 		cubo.position.x=10*i;
 		cubo.position.z=10*j;
 		grupo.add(cubo);}
 		else{
+		var forma = new THREE.BoxGeometry( 10,.1,10);
 		var material = new THREE.MeshBasicMaterial( {color: 0xffffff}); //0xcfcfcf
 		var cubo = new THREE.Mesh(forma, material);
 		cubo.position.x=10*i;
