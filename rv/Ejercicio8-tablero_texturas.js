b486f25153304dbdd18a0461d5b66e9aaf4c1900
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-15 01:48:53 -0500

    Update Ejercicio8-tablero_texturas.js

--- rv/Ejercicio8-tablero_texturas.js
+++ rv/Ejercicio8-tablero_texturas.js
@@ -1,41 +1,28 @@
 for(var i=1; i<=8; i++)
 {
    for(var j=1; j<=8; j++)
-   {	
+   {
+	var forma = new THREE.BoxGeometry( 10,.1,10);
+	
 	if(j%2 ==0)
 	{
-		if(i%2 == 0){
-		var forma = new THREE.BoxGeometry( 10,.1,10);
-		var material = new THREE.MeshBasicMaterial( {color: 0xffffff});
-		var cubo = new THREE.Mesh(forma, material);
-		cubo.position.x=10*i;
-		cubo.position.z=10*j;
-		grupo.add(cubo);}
-		else{
-		var forma = new THREE.BoxGeometry( 10,.1,10);
+		if(i%2 == 0)
+		var material = new THREE.MeshBasicMaterial( {color: 0xffffff}); 
+		else
 		var material = new THREE.MeshBasicMaterial( {color: 0x727272}); //0xcfcfcf
-		var cubo = new THREE.Mesh(forma, material);
-		cubo.position.x=10*i;
-		cubo.position.z=10*j;
-		grupo.add(cubo);}
 	}
 	else
 	{
-		if(i%2 == 0){
-		var forma = new THREE.BoxGeometry( 10,.1,10);
+		if(i%2 == 0)
 		var material = new THREE.MeshBasicMaterial( {color: 0x727272}); //0xcfcfcf
-		var cubo = new THREE.Mesh(forma, material);
-		cubo.position.x=10*i;
-		cubo.position.z=10*j;
-		grupo.add(cubo);}
-		else{
-		var forma = new THREE.BoxGeometry( 10,.1,10);
+		else
 		var material = new THREE.MeshBasicMaterial( {color: 0xffffff}); //0xcfcfcf
-		var cubo = new THREE.Mesh(forma, material);
-		cubo.position.x=10*i;
-		cubo.position.z=10*j;
-		grupo.add(cubo);}
 	}
+	
+	var cubo = new THREE.Mesh(forma, material);
+	cubo.position.x=10*i;
+	cubo.position.z=10*j;
+	escena.add(cubo);
    }
 }
 /*var i,j;
