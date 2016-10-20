5b979be7442e9eddd0b62b3febcfad60e534d8b3
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-20 15:12:36 -0500

    Update ejemplo-pieza-mov.js

--- rv/ejemplo-pieza-mov.js
+++ rv/ejemplo-pieza-mov.js
@@ -27,7 +27,7 @@ function setup()
       pieza = new Pieza();
       
       escena = new THREE.Scene();
-    
+      escena.add(pieza);
       camara= new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
       
       camara.position.z=25;
