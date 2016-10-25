11cd001c0da307c18f8b17b6b74eda041a9a6f2b
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-25 15:46:30 -0500

    Update ejemplo-Pelotas_mov.js

--- rv/ejemplo-Pelotas_mov.js
+++ rv/ejemplo-Pelotas_mov.js
@@ -84,7 +84,7 @@ Pelota.prototype.act = function(environment)
       this.position.x += this.step;
 }
 
-fucntion Pared (seze, x=0, y=0)
+fucntion Pared (size, x=0, y=0)
 {
       THREE.Object3D.call(this,x,y);
       
