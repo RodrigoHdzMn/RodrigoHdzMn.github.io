8402b65295ee12425a7fab645c93838dbad930fb
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-25 15:51:52 -0500

    Update ejemplo-Pelotas_mov.js

--- rv/ejemplo-Pelotas_mov.js
+++ rv/ejemplo-Pelotas_mov.js
@@ -2,8 +2,8 @@
 function Agent(x=0, y=0)
 {
       THREE.Object3D.call(this);
-      this.postion.x =x;
-      this.postion.y =y;     
+      this.position.x =x;
+      this.position.y =y;     
 }
 
 Agent.prototype = new THREE.Object3D();
