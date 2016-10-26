0b0a7009257984153f2348de0197f2eeef2099c6
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-26 03:00:40 -0500

    Update ejemplo-Pelotas_mov.js

--- rv/ejemplo-Pelotas_mov.js
+++ rv/ejemplo-Pelotas_mov.js
@@ -65,8 +65,8 @@ Pelota.prototype.sense = function(environment)
       this.sensor.set(this.position, new THREE.Vector3(1,0,0));
       var obstaculo1 = this.sensor.intersectObjects(environment.children,true);
       
-      this.sensor.set(this.position, new THREE.Vector3(-1,0,0));
-      var obstaculo2 = this.sensor.intersectObjects(environment.children,true);
+      this.sensor2.set(this.position, new THREE.Vector3(-1,0,0));
+      var obstaculo2 = this.sensor2.intersectObjects(environment.children,true);
       
       if( (obstaculo1.length > 0 && (obstaculo1[0].distance <= this.radius)) ||
           (obstaculo2.length > 0 && (obstaculo2[0].distance <= this.radius))  )
