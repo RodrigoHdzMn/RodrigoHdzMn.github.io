955fefeaeb18938cf33a197fd4f0cfb70d57ce5b
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-10-27 15:41:33 -0500

    Update ejemplo-Agente_Robot.js

--- rv/ejemplo-Agente_Robot.js
+++ rv/ejemplo-Agente_Robot.js
@@ -45,6 +45,61 @@ Environment.prototype.setMap =  function(map)
       }
 }
 
+function Sensor(position,direction)
+{
+      THREE.Raycaster.call( this, position, direction );
+      this.colision =  false;
+}
+Sensor.prototype = new THREE.Raycaster();
+
+function Robot(size, x, y)
+{
+      Agent.call(this, x, y);
+      
+      this.sensor = new Sensor();
+      this.actuador = new THREE.Mesh(
+                          new THREE.BoxGeometry(size,size,size),
+                          new THREE.MeshBasicMaterial( {color:'#aa0000'} ) );
+      this.actuador.commands = [];
+      this.add(this.actuador);
+}
+Robot.prototype = new Agent();
+
+Robot.prototype.sense = function(environment)
+{
+      this.sensor.set( this.position,
+                       new THREE.Vector3( Math.cos(this.rotation.z), Math.sin(this.rotation.z), 0) );
+      var obstaculo = this.sensor, intersectObjects(environment.children, true);
+      
+      if( (obstaculo.length > 0 && (obstaculo[0].distance <= 0.5)) )
+         this.sensor.colision = true;
+         else
+         this.sensor.colision = false;
+}
+
+Robot.prototype.plan = function(environment)
+{
+      this.actuador.commands = [];
+      
+      if( this.sensor.colision == true )
+            this.actuador.commands.push('rotateCCW');
+      else
+            this.actuador.commands.push('goStraight');
+}
+
+Robot.prototype.act(environment)
+{
+      var command = this.actuador.commands.pop();
+      
+      if(command === undefined)
+            console.log('Undefined command');
+      else if ( command in this.operations )
+            this.operations[command] (this);
+      else
+            console.log('Unknow command');
+}
+         
+
 function setup()
 {
       var mapa = new Array();
