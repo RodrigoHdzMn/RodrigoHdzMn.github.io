14ef7be229cf23218a5fc432da36e7c552a783c7
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-11-15 15:55:43 -0600

    Update ejemplo-Agente_Robot.js

--- rv/ejemplo-Agente_Robot.js
+++ rv/ejemplo-Agente_Robot.js
@@ -115,7 +115,7 @@ Robot.prototype.plan = function(environment)
 
 Robot.prototype.act(environment)
 {
-      var command = this.actuador.commands.pop();
+      var command = this.actuator.commands.pop();
       
       if(command === undefined)
             console.log('Undefined command');
