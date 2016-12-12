0286d543b72b203321261ab131734d9c8ca9a9cf
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-12-11 23:56:00 -0600

    Update Ejercicio9-mov_pieza.js

--- rv/Ejercicio9-mov_pieza.js
+++ rv/Ejercicio9-mov_pieza.js
@@ -123,7 +123,7 @@ function init(){
     
 torre1=new THREE.Mesh(torreForma,marmolblanco);
     torre1.position.y=10;
-    torre1.position.z=-7;
+   // torre1.position.z=-7;
     torre1.position.x=10;
     //torre1.scale.set(0.04,0.04,0.04);
     torre1.rotateX(-Math.PI/2);
