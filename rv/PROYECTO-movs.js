b6476486d8a5e0eb3883aabbaebd30f48e1c2490
RodrigoHdzMn <rodrigo10tmm@hotmail.com>
2016-12-19 10:13:52 -0600

    Update PROYECTO-movs.js

--- rv/PROYECTO-movs.js
+++ rv/PROYECTO-movs.js
@@ -3811,7 +3811,7 @@ function init() {
   camara = new THREE.PerspectiveCamera();
   camara.position.y = 160;
   camara.position.x = 40;
-  camara.position.z = -20;
+  camara.position.z = -120;
   camara.lookAt(new THREE.Vector3(40, 0, 40));
   ///////////////////////////////////////////Renderizador//////////////////////////////////////////////////////////////////////////
   renderizador = new THREE.WebGLRenderer({antialias:true});
